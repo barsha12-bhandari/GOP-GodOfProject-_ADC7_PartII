1995855f21035231372723969f799fbf4eba3aee
Saransh34 <rjsaransh34@gmail.com>
2020-02-09 12:19:21 +0545

    Added model relationships, many to many

---
+++ dogSeller/cartAndOrder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CartandorderConfig(AppConfig):
+    name = 'cartAndOrder'
---
+++ dogSeller/cartAndOrder/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from viewDogs.models import *
+from django.contrib.auth.models import User
+
+class Order(models.Model):
+    product = models.ManyToManyField(Dogs,blank=True)
+    customer = models.ManyToManyField(User,blank=True)
+    quantity = models.IntegerField()
+    deliveryLocation = models.CharField(max_length=200)
+    def __str__(self):
+        return self.id
+
+class Cart(models.Model):
+    product = models.ManyToManyField(Dogs,blank=True)
+    customer = models.ManyToManyField(User,blank=True)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.id
+
